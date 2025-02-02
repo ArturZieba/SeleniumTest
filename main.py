049649699dacfcea388bf3b8dfe0da48eccc896f
Siwy <43797135+ArturZieba@users.noreply.github.com>
2025-02-02 14:22:07 +0100

    Getting and interacting with webpage elements
    - Test interaction with webpage elements.

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 service = Service(executable_path="chromedriver.exe") # chromedriver.exe downloaded from https://sites.google.com/chromium.org/driver/
 driver = webdriver.Chrome(service=service)
 
 driver.current_window_handle
+
+# driver.get("<webpage>")
+
+# time.sleep(3)
+
+# input_element_reject = driver.find_element(By.CLASS_NAME, "<class>")
+# input_element_reject.send_keys(Keys.ENTER)
+
+# input_element = driver.find_element(By.CLASS_NAME, "<class>")
+# input_element.send_keys("test 123" + Keys.ENTER)
 
 time.sleep(10)
 
