454f8fb4504829fd5f656fe33729b18c397274f3
Siwy <43797135+ArturZieba@users.noreply.github.com>
2025-02-15 15:24:53 +0100

    Add tests for clicking button elements
    - Add tests for clicking button elements.

--- main.py
+++ main.py
@@ -11,13 +11,29 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("http://localhost:8000")
 
-time.sleep(3)
+time.sleep(2)
 
+# Open the page with Selenium test elements
 link_to_test_page = driver.find_element(By.LINK_TEXT, "Selenium Test Page")
 link_to_test_page.click()
 
-# input_element = driver.find_element(By.CLASS_NAME, "<class>")
-# input_element.send_keys("test 123" + Keys.ENTER)
+# Click the button elements
+# st_red_button = driver.find_element(By.CLASS_NAME, "button-red")
+# st_red_button.click()
+
+# time.sleep(2)
+
+# st_green_button = driver.find_element(By.CLASS_NAME, "button-green")
+# st_green_button.click()
+
+# time.sleep(2)
+
+# st_blue_button = driver.find_element(By.CLASS_NAME, "button-blue")
+# st_blue_button.click()
+
+# Go back to the main page
+link_to_test_page = driver.find_element(By.LINK_TEXT, "AZ Main Page")
+link_to_test_page.click()
 
 time.sleep(10)
 
