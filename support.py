a07f311ba76e2a26b44ea866a207cbb4985ead09
Siwy <43797135+ArturZieba@users.noreply.github.com>
2025-04-09 09:35:13 +0200

    Create support.py to house FontModifiers class

---
+++ support.py
@@ -0,0 +1,12 @@
+# Class with ANSI escape codes
+class FontModifiers:
+    DEFAULT = '\033[0m' # Restore default font
+    BOLD = '\033[1m' # Make font bold
+    BLACK = '\033[90m' # Change font color to black
+    RED = '\033[91m' # Change font color to red
+    GREEN = '\033[92m' # Change font color to green
+    YELLOW = '\033[93m' # Change font color to yellow
+    BLUE = '\033[94m' # Change font color to blue
+    MAGENTA = '\033[95m' # Change font color to magenta
+    CYAN = '\033[96m' # Change font color to cyan
+    WHITE = '\033[97m' # Change font color to white
