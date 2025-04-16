1d23709cd09b71765ac45dcbd7c66a4b430ce6b7
Siwy <43797135+ArturZieba@users.noreply.github.com>
2025-04-16 09:38:07 +0200

    Move font modifiers into functions
    - Move font modifiers into separate functions for more convenient use.

--- support.py
+++ support.py
@@ -11,16 +11,55 @@ class FontModifiers:
     CYAN = '\033[96m' # Change font color to cyan
     WHITE = '\033[97m' # Change font color to white
 
-# Add a wrapper that returns modified string without having to add the values manually?
+def bold(text):
+    print(FontModifiers.BOLD + text + FontModifiers.DEFAULT)
+
+def black(text):
+    print(FontModifiers.BLACK + text + FontModifiers.DEFAULT)
+
+def red(text):
+    print(FontModifiers.RED + text + FontModifiers.DEFAULT)
+
+def green(text):
+    print(FontModifiers.GREEN + text + FontModifiers.DEFAULT)
+
+def yellow(text):
+    print(FontModifiers.YELLOW + text + FontModifiers.DEFAULT)
+
+def blue(text):
+    print(FontModifiers.BLUE + text + FontModifiers.DEFAULT)
+
+def magenta(text):
+    print(FontModifiers.MAGENTA + text + FontModifiers.DEFAULT)
+
+def cyan(text):
+    print(FontModifiers.CYAN + text + FontModifiers.DEFAULT)
+
+def white(text):
+    print(FontModifiers.WHITE + text + FontModifiers.DEFAULT)
+
+def bold_red(text):
+    print(FontModifiers.BOLD + FontModifiers.RED + text + FontModifiers.DEFAULT)
 
 # Test of all font options:
-print(FontModifiers.BOLD + "BOLD" + FontModifiers.DEFAULT)
-print(FontModifiers.BLACK + "BLACK" + FontModifiers.DEFAULT)
-print(FontModifiers.RED + "RED" + FontModifiers.DEFAULT)
-print(FontModifiers.GREEN + "GREEN" + FontModifiers.DEFAULT)
-print(FontModifiers.YELLOW + "YELLOW" + FontModifiers.DEFAULT)
-print(FontModifiers.BLUE + "BLUE" + FontModifiers.DEFAULT)
-print(FontModifiers.MAGENTA + "MAGENTA" + FontModifiers.DEFAULT)
-print(FontModifiers.CYAN + "CYAN" + FontModifiers.DEFAULT)
-print(FontModifiers.WHITE + "WHITE" + FontModifiers.DEFAULT)
-print(FontModifiers.BOLD + FontModifiers.RED + "BOLD RED" + FontModifiers.DEFAULT)
+bold("BOLD")
+black("BLACK")
+red("RED")
+green("GREEN")
+yellow("YELLOW")
+blue("BLUE")
+magenta("MAGENTA")
+cyan("CYAN")
+white("WHITE")
+bold_red("BOLD RED")
+
+# print(FontModifiers.BOLD + "BOLD" + FontModifiers.DEFAULT)
+# print(FontModifiers.BLACK + "BLACK" + FontModifiers.DEFAULT)
+# print(FontModifiers.RED + "RED" + FontModifiers.DEFAULT)
+# print(FontModifiers.GREEN + "GREEN" + FontModifiers.DEFAULT)
+# print(FontModifiers.YELLOW + "YELLOW" + FontModifiers.DEFAULT)
+# print(FontModifiers.BLUE + "BLUE" + FontModifiers.DEFAULT)
+# print(FontModifiers.MAGENTA + "MAGENTA" + FontModifiers.DEFAULT)
+# print(FontModifiers.CYAN + "CYAN" + FontModifiers.DEFAULT)
+# print(FontModifiers.WHITE + "WHITE" + FontModifiers.DEFAULT)
+# print(FontModifiers.BOLD + FontModifiers.RED + "BOLD RED" + FontModifiers.DEFAULT)
