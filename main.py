b12a16510850c4790a6eacac2744adf1fea5a10a
Siwy <43797135+ArturZieba@users.noreply.github.com>
2025-04-21 15:52:07 +0200

    Test basic unit tests
    - Create a basic unit test with a try/except block.
    - Add setUp and tearDown functions.
    - Verify added functions.

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import stpunittests
+import unittest
+
+class TestUnitTesting(unittest.TestCase):
+    def setUp(self):
+        print("Running unit tests")
+
+    def test_try(self):
+        try:
+            print("Works")
+        except:
+            assert False, "Didn't do something"
+    
+    def tearDown(self):
+        print("Finished running unit tests")
+
+        
+if __name__ == '__main__':
+    unittest.main()
 
 # Open the browser on http://localhost:8000
-stpunittests.launch_browser()
+#stpunittests.launch_browser()
 
 # Open the page with Selenium test elements
-stpunittests.main_to_stp()
+#stpunittests.main_to_stp()
 
 ##### 
 
@@ -21,28 +39,29 @@ stpunittests.main_to_stp()
 #####
 
 # Click the button elements
-stpunittests.click_buttons()
+#stpunittests.click_buttons()
 
 # Type something into a search bar and opy contents of one search bar into another
-stpunittests.type_in_search_bar()
+#stpunittests.type_in_search_bar()
 
 # Click through the radio buttons
-stpunittests.click_radio_buttons()
+#stpunittests.click_radio_buttons()
 
 # Go through all of the options on a select list
-stpunittests.select_list_select_all_options()
+#stpunittests.select_list_select_all_options()
 
 # Enter log in information
-stpunittests.use_login()
+#stpunittests.use_login()
 
 # Download an image
-stpunittests.download_image()
+#stpunittests.download_image()
 
 # Play through different audio elements
-stpunittests.play_audio_elements()
+#stpunittests.play_audio_elements()
 
 # Go back to the main page
-stpunittests.stp_to_main()
+#stpunittests.stp_to_main()
 
 # Confirm execution of the whole file
-stpunittests.finish_test_run()
+#stpunittests.finish_test_run()
+
